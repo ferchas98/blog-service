bae5803c2fdedcd3c043e2e01ed1e32b0ffc5b40
fernando <fer_oca98@hotmail.com>
2024-06-07 20:12:11 -0600

    fix2

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./src/app");
 
-// Establece el puerto, usa el puerto 3000 por defecto si no se especifica un puerto en las variables de entorno
-const PORT = process.env.PORT || 3000;
+// Establece el puerto, usa el puerto por defecto si no se especifica un puerto en las variables de entorno
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
