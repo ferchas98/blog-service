7ce3093ef6e27c0111917dd693b4742a67a26f2e
fernando <fer_oca98@hotmail.com>
2024-06-09 20:22:42 -0600

    change auth.js

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const createError = require("http-errors");
 
 module.exports = (req, res, next) => {
-  const token = req.header("Authorization").replace("Bearer ", "");
+  const token = req.header("Authorization").replace("token", "");
   if (!token) {
     return next(createError(401, "Access denied. No token provided."));
   }
